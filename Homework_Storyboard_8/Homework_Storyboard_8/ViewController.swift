ffe254f1f10a98f20a01ad35cc4ac1922668a9a8
NastyaGaranovich <nastyamadness@gmail.com>
2021-03-17 19:46:21 +0300

    Create project Add .gitignore

---
+++ Homework_Storyboard_8/Homework_Storyboard_8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_Storyboard_8
+//
+//  Created by Анастасия Гаранович on 17.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
